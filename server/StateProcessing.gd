00dc35abf046f1f25696bc3520678313ef78c753
Jonathan Green <dev.jonathan.green@gmail.com>
2022-06-13 00:04:49 -0400

    WIP

--- server/StateProcessing.gd
+++ server/StateProcessing.gd
@@ -4,10 +4,6 @@ var world_state = {}
 
 func _physics_process(delta):
 	if not Server.player_state.empty() or not Server.decoration_state.empty():
-		#for player_id in Server.player_state.keys():
-			#var player = Server.world.get_node(player_id)
-			#player.position = Server.player_state[player_id]["P"]
-			#player.directon = Server.player_state[player_id]["D"]
 		world_state["player_state"] = Server.player_state.duplicate(true)
 		world_state["decoration_state"] = Server.decoration_state.duplicate(true)
 		for player in world_state["player_state"].keys():
