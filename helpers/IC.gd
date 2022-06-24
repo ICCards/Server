309f25b8fb8ee847ba7193adb8a7a77b8615c4ea
Jonathan Green <dev.jonathan.green@gmail.com>
2022-06-24 16:58:17 -0400

    checking principal

--- helpers/IC.gd
+++ helpers/IC.gd
@@ -4,17 +4,20 @@ var this = self
 var url = "https://nheqc-cqaaa-aaaan-qam6a-cai.raw.ic0.app"
 var endpoint = "player"
 var rng = RandomNumberGenerator.new()
-var http_request = HTTPRequest.new()
 
 func _ready():
-	add_child(http_request)
+	pass
 
 func principal(player_id):
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
 	rng.randomize()
 	var random_delay = rng.randf_range(0.1, 0.9)
 	yield(get_tree().create_timer(random_delay), "timeout")
 	http_request.connect("request_completed", this, "_principal_request_completed")
 	http_request.request(url+"/"+endpoint+"/"+str(player_id))
+	yield(get_tree().create_timer(5), "timeout")
+	http_request.queue_free()
 
 func _principal_request_completed(result, response_code, headers, body):
 	print("response_code")
@@ -29,4 +32,4 @@ func _principal_request_completed(result, response_code, headers, body):
 				print("got principal")
 				print(json[key])
 				Server.players[key]["principal"] = json[key]
-				print(Server.players[key]["principal"])
+				print(Server.players[key])
